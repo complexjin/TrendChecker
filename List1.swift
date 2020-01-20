177772c01efe75d7f2e914bbc29795c25b2cf194
complexjin <59429747+complexjin@users.noreply.github.com>
2020-01-20 14:42:10 +0900

    Add files via upload

---
+++ List1.swift
@@ -0,0 +1,14 @@
+//
+//  List1.swift
+//  TrendChecker2
+//
+//  Created by user on 2020/01/20.
+//  Copyright © 2020 dd. All rights reserved.
+//
+
+import UIKit
+class List1 : UITableViewCell{
+
+    
+    @IBOutlet weak var KeyWordList: UILabel!
+}
